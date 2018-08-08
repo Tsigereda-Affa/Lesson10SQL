96133f5862bd4dd3c181aee4356fce3216b744e3
tsigereda-affa <affa16.a@gmail.com>
2018-08-08 16:39:19 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot10MysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10MysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot10MysqlApplication.class, args);
+	}
+}
